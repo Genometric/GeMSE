2c6adff98683ef55531fecaef4f44e71ca41ac60
vahid <jalili.vahid@gmail.com>
2017-08-22 21:42:22 -0700

    Removed an unused import.

--- GeMSE/src/main/java/GeMSE/Visualization/Graph/GraphVis.java
+++ GeMSE/src/main/java/GeMSE/Visualization/Graph/GraphVis.java
@@ -34,7 +34,6 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
